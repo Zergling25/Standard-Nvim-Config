e6a7f6ae8a2dffb4cac2f8239fa39f3f24a2abe2
Zergling25 <mharrie@oakland.edu>
2025-02-18 22:14:08 -0500

    The first commit

---
+++ nvim/lua/custom/plugins/alpha.lua
@@ -0,0 +1,85 @@
+local config = function()
+  local header = {
+    type = 'text',
+    val = {
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+    },
+    opts = {
+      position = 'center',
+      hl = 'Type',
+    },
+  }
+
+  local version = vim.version()
+
+  local section = {
+    header = header,
+    version = {
+      type = 'text',
+      val = string.format('NVIM v%s.%s.%s-%s %s', version.major, version.minor, version.patch, version.prerelease, version.build),
+      opts = {
+        position = 'center',
+        hl = 'Ignore',
+      },
+    },
+    fortune = {
+      type = 'text',
+      val = '', -- Set during config as we can't require alpha in lazy opts
+      opts = {
+        position = 'center',
+        hl = 'Comment',
+      },
+    },
+    lazy = {
+      type = 'text',
+      val = '', -- Set by autocommand created in lazy config
+      opts = {
+        position = 'center',
+        hl = 'Ignore',
+      },
+    },
+  }
+
+  local dashboard = {
+    section = section,
+    config = {
+      layout = {
+        { type = 'padding', val = 1 },
+        section.header,
+        { type = 'padding', val = 2 },
+        section.version,
+        { type = 'padding', val = 1 },
+        section.fortune,
+        { type = 'padding', val = 2 },
+        section.lazy,
+      },
+    },
+  }
+
+  dashboard.section.fortune.val = require 'alpha.fortune'(60)
+
+  require('alpha').setup(dashboard.config)
+
+  vim.api.nvim_create_autocmd('User', {
+    callback = function()
+      local stats = require('lazy').stats()
+      local ms = math.floor(stats.startuptime * 100) / 100
+      dashboard.section.lazy.val = 'Lazy-loaded ' .. stats.loaded .. ' of ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
+end
+
+return {
+  'goolord/alpha-nvim',
+  lazy = false,
+  config = config,
+}

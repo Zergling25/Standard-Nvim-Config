96fcae44f407c4d6c83d80a2fc5a3b27c0eef39a
Zergling25 <mharrie@oakland.edu>
2025-07-20 18:34:19 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- STILL NEED TO SET-UP LSP for python and CPP as well as FORMATTING (AND MAYBE LINTING)
+require 'keymaps'
+require 'functions'
+require 'settings'
+require 'statusline'
+require 'tabs'
+require 'terminal'
+require 'theme'
+require 'lazy-bootstrap'
+require 'lazy-plugins'
+
+-- Activate LSP(s)
+vim.lsp.enable('luals')
+
